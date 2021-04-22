000e27d7d82458d5249aa782b033485f397786b4
rebecca dmello <rebeccadmello1996@gmail.com>
2021-04-22 13:59:32 -0700

    first commit

--- AddFragmentToActivityUsingXML/app/src/main/java/com/example/addfragmenttoactivityusingxml/HelloFragment.java
+++ AddFragmentToActivityUsingXML/app/src/main/java/com/example/addfragmenttoactivityusingxml/HelloFragment.java
@@ -1,17 +1,15 @@
-package com.example.reza.addfragmenttoanactivityusingxml;
+package com.example.addfragmenttoactivityusingxml;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by Reza on 2018-02-18.
- */
+import androidx.annotation.Nullable;
 
-public class HelloFragment extends Fragment{
+
+public class HelloFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
--- AddFragmentToActivityUsingXML/app/src/main/java/com/example/addfragmenttoactivityusingxml/MainActivity.java
+++ AddFragmentToActivityUsingXML/app/src/main/java/com/example/addfragmenttoactivityusingxml/MainActivity.java
@@ -1,7 +1,8 @@
-package com.example.reza.addfragmenttoanactivityusingxml;
+package com.example.addfragmenttoactivityusingxml;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
